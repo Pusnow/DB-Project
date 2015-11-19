f3f439b7bb87c69c3678e0e6fdcc4c56dea3ae39
Wonsup Yoon <pusnow@yonsei.ac.kr>
2015-11-19 17:48:37 +0900

    Model Design, generate instance tables

--- DBP/models/instance.py
+++ DBP/models/instance.py
@@ -2,3 +2,18 @@
 
 
 from DBP.models import Base
+
+
+
+class OriginalData (object):
+
+	pass
+
+
+
+class TaskData (object):
+	pass
+
+
+class ParsedData (object):
+	pass
--- DBP/models/task.py
+++ DBP/models/task.py
@@ -1,9 +1,16 @@
 #-*- coding: utf-8 -*-
 
 
-from DBP.models import Base
-from sqlalchemy import Column,  Unicode, Text, DateTime
+from DBP.models import Base, engine
+from sqlalchemy import Column,  Unicode, Text, DateTime, Integer, BLOB, ForeignKey, Enum
 from sqlalchemy.orm import relationship
+from sqlalchemy import Table, MetaData
+from DBP.models.instance import OriginalData, TaskData, ParsedData
+
+from DBP.models.user import User
+
+
+
 
 class Task(Base):
 	__tablename__ = 'Task'
@@ -11,3 +18,54 @@ class Task(Base):
 	information = Column(Text)
 	duration = Column(DateTime)
 	prefix = Column(Unicode(10), unique = True)
+
+
+	def __init__(self,name,prefix):
+		self.name = name[:100]
+		self.prefix = prefix[:10]
+
+
+
+	def generateTables(self,mappinginfo):
+		metadata = Base.metadata
+		#original data
+
+		originalTableName = self.prefix + "_OriginalData"
+		
+		original = Table(originalTableName,metadata,
+			Column('id', Integer, primary_key=True),
+			*(Column(colname, Integer) for colname in mappinginfo)
+			)
+
+
+		#parsed data
+		parsedTableName = self.prefix + "_ParsedData"
+
+		parsed = Table(parsedTableName,metadata,
+			Column('id', Integer, primary_key=True),
+			Column('duration', DateTime),
+			Column('count', Integer),
+			Column('file',BLOB),
+			Column('tuplenum',Integer),
+			Column('duplicatetuplenum',Integer),
+			Column('originalid', Integer, ForeignKey(originalTableName+'.id')),
+			Column('evaluator', Integer, ForeignKey('User.id')),
+			Column('status',Enum('Waiting','Evaluated')),
+			Column('score',Integer),
+			Column('pnp',Enum('Pass','Nonpass')),
+			Column('submitter', Integer, ForeignKey('User.id'))
+
+			)
+
+
+		#task data
+		
+		taskTableName = self.prefix + "_TaskData"
+		parsed = Table(taskTableName,metadata,
+			Column('id', Integer, primary_key=True),
+			Column('submittername', Unicode(100)),
+			Column('parsedid', Integer, ForeignKey(parsedTableName+'.id')),
+			*(Column(colname, Integer) for colname in mappinginfo)
+			)
+
+		metadata.create_all(bind= engine)
