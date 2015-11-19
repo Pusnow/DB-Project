0dc6f33fb44ee8e699e4996af972becc07db6285
Wonsup Yoon <pusnow@yonsei.ac.kr>
2015-11-20 02:22:12 +0900

    test code

---
+++ test.py
@@ -0,0 +1,17 @@
+
+
+from DBP.models import Base,engine, session
+from sqlalchemy import  MetaData
+
+from DBP.models.task import Task
+from DBP.models.user import User, Enroll
+
+
+Base.metadata.create_all(engine)
+
+t = Task("test","TS")
+
+
+d = ["data1","data2","data3"]
+
+t.generateTables(d)
