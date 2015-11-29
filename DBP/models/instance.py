aabaa718034e12e868b806bafae17d78cc338860
Wonsup Yoon <pusnow@yonsei.ac.kr>
2015-11-29 16:01:35 +0900

    OriginalData Model 생성 및 CSV 로드기능

--- DBP/models/instance.py
+++ DBP/models/instance.py
@@ -1,16 +1,87 @@
 #-*- coding: utf-8 -*-
 
 
-from DBP.models import Base
-
+from DBP.models import Base, session
+from DBP.models.user import randomEvaluator
+from sqlalchemy.orm import class_mapper
+from sqlalchemy.inspection import inspect
+import csv
+import io
 
 
 class OriginalData (object):
 
-	__table_args__  = {'autoload':True}
-	def test(self):
-		print self.__table__
-	pass
+	
+
+
+	def __init__(self, length, name, mappinginfo):
+		self.length = length
+		self.name  = name
+		cols = inspect(self.__class__).columns
+		if len(mappinginfo) != len(cols) -3:
+			raise TypeError
+
+		for col in cols:
+
+			if not col.name[:3] == u"sch":
+				continue
+
+
+			if int(mappinginfo[col.name]) >= self.length :
+				raise TypeError
+			setattr(self,col.name,int(mappinginfo[col.name]))
+
+
+	def mapList(self):
+		maplist = list()
+		for col in inspect(self.__class__).columns:
+			if not col.name[:3] == u"sch":
+				continue
+
+
+			maplist.append(getattr(self,col.name))
+		return maplist
+
+
+	def loadcsv(self,submitter,csvread,nth,duration_start,duration_end):
+		reader = csv.reader(csvread, delimiter=',', quotechar="'")
+		csvwrite = io.BytesIO()
+		writer =  csv.writer(csvwrite, delimiter=',', quotechar="'")
+		maplist = self.mapList()
+		counter = 0
+		dupset = set()
+		dupcounter = 0
+		for rrow in reader:
+			crow = list()
+			dupset.add(unicode(rrow))
+			for mapnum in maplist:
+				crow.append(unicode(rrow[mapnum]))
+
+			writer.writerow(crow)
+			counter += 1
+
+
+		evaluator = randomEvaluator()
+		
+		parsedmodel =  self.parsedclass(nth,duration_start,duration_end,csvwrite,counter, counter - len(dupset))
+		parsedmodel.submitter = submitter.id
+		parsedmodel.evaluator = evaluator.id
+		self.parsed.append(parsedmodel)
+
+		session.commit()
+
+
+
+class ParsedData (object):
+
+	def __init__(self,nth,duration_start,duration_end, csvfile, tuplenum,duplicatetuplenum):
+		self.nth = nth
+		self.duration_start = duration_start
+		self.duration_end = duration_end
+		self.file = csvfile
+		self.tuplenum = tuplenum
+		self.duplicatetuplenum = duplicatetuplenum
+
 
 
 
@@ -18,5 +89,3 @@ class TaskData (object):
 	pass
 
 
-class ParsedData (object):
-	pass
