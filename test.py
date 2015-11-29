a26a2e821e76899f8d8da434f2f7c7f91885ff71
Wonsup Yoon <pusnow@yonsei.ac.kr>
2015-11-29 16:04:42 +0900

    추가 기능 테스트 코드

--- test.py
+++ test.py
@@ -1,13 +1,22 @@
-
+#-*- coding: utf-8 -*-
 
 from DBP.models import Base,engine, session
 from sqlalchemy import  MetaData
-
+from datetime import datetime
 from DBP.models.task import Task
 from DBP.models.user import User, Enroll
-
+import io
 
 Base.metadata.create_all(engine)
+str(datetime.now())
+evaluator = User(u"evaluator" + str(datetime.now()),u"김똘똘",u"1234")
+evaluator.role = u"평가자"
+
+submitter = User(u"submitter" + str(datetime.now()),u"김똘똘2",u"1234")
+
+session.add(evaluator)
+session.add(submitter)
+session.commit()
 
 t = Task("test","TS")
 
@@ -15,6 +24,21 @@ t = Task("test","TS")
 d = ["data1","data2","data3"]
 
 t.generateTables(d)
-a =  t.original()
-print dir(a)
+mapinfo = {
+	"sch_data1" : 3 ,
+	"sch_data2" : 1 ,
+	"sch_data3" : 5}
+name = u"하나카드"
+length = 6
 t.setTables()
+om = t.newOriginal(length,name,mapinfo)
+
+
+csvdata = u"""'1','45','32','23','21','312'
+'1','45','32','23','21','312'
+'1','45','32','23','21','312'
+'1','45','32','23','21','312'
+'1','45','32','23','21','312'
+"""
+csvtest = io.StringIO(csvdata)
+om.loadcsv(submitter,csvtest,1,datetime(2015, 1, 1),datetime(2016, 1, 1))
