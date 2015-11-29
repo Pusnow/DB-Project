0eaee4d02d7ded2d64a468be3b12695504e46bb5
Wonsup Yoon <pusnow@yonsei.ac.kr>
2015-11-29 16:03:44 +0900

    relationship추가, relationship class 추가, original table 스키마 수정, parsed table 스키마 수정, auto increment 추가, 오리지덜데이터 생성 메서드, 매핑 정보 반환 메서드

--- DBP/models/task.py
+++ DBP/models/task.py
@@ -1,7 +1,6 @@
 #-*- coding: utf-8 -*-
 
-
-from DBP.models import Base, engine
+from DBP.models import Base, engine, session
 from sqlalchemy import Column,  Unicode, Text, DateTime, Integer, BLOB, ForeignKey, Enum
 from sqlalchemy.orm import relationship
 from sqlalchemy import Table, MetaData
@@ -31,16 +30,23 @@ class Task(Base):
 		originalTableName = self.prefix + "_OriginalData"
 		parsedTableName = self.prefix + "_ParsedData"
 		taskTableName = self.prefix + "_TaskData"
-		original = Table(originalTableName, metadata, autoload=True, autoload_with=engine)
-		parsed = Table(parsedTableName, metadata, autoload=True, autoload_with=engine)
-		task = Table(taskTableName, metadata, autoload=True, autoload_with=engine)
+		self.originaltable = Table(originalTableName, metadata, autoload=True, autoload_with=engine)
+		self.parsedtable = Table(parsedTableName, metadata, autoload=True, autoload_with=engine)
+		self.tasktable = Table(taskTableName, metadata, autoload=True, autoload_with=engine)
 
 		self.original = type(originalTableName,(OriginalData,),{})
 		self.parsed = type(parsedTableName,(ParsedData,),{})
 		self.task = type(taskTableName,(TaskData,),{})
-		mapper(self.original, original)
-		mapper(self.parsed, parsed)
-		mapper(self.task, task)
+		mapper(self.original, self.originaltable, properties={
+			'parsed': relationship(self.parsed, backref='original')
+		})
+		mapper(self.parsed, self.parsedtable, properties={
+			'task': relationship(self.task, backref='parsed')
+		})
+		mapper(self.task, self.tasktable)
+
+		self.original.parsedclass = self.parsed
+		self.parsed.taskclass = self.task
 
 
 
@@ -50,42 +56,45 @@ class Task(Base):
 
 		originalTableName = self.prefix + "_OriginalData"
 		
-		original = Table(originalTableName,metadata,
-			Column('id', Integer, primary_key=True),
+		self.originaltable = Table(originalTableName,metadata,
+			Column('id', Integer, primary_key=True,autoincrement = True),
+			Column('length', Integer),
+			Column('name', Unicode(100)),
 			*(Column("sch_"+colname, Integer) for colname in mappinginfo)
-			,autoload=True, autoload_with=engine
+			#,autoload=True, autoload_with=engine
 			)
 
 
 		#parsed data
 		parsedTableName = self.prefix + "_ParsedData"
 
-		parsed = Table(parsedTableName,metadata,
-			Column('id', Integer, primary_key=True),
-			Column('duration', DateTime, nullable = False),
-			Column('count', Integer, nullable = False, server_default = "0"),
+		self.parsedtable = Table(parsedTableName,metadata,
+			Column('id', Integer, primary_key=True,autoincrement = True),
+			Column('duration_start', DateTime, nullable = False),
+			Column('duration_end', DateTime, nullable = False),
+			Column('nth', Integer, nullable = False, server_default = "1"),
 			Column('file',BLOB, nullable = False),
 			Column('tuplenum',Integer, nullable = False),
 			Column('duplicatetuplenum',Integer, nullable = False),
 			Column('originalid', Integer, ForeignKey(originalTableName+'.id'), nullable = False),
-			Column('evaluator', Unicode(30), ForeignKey('User.id'), nullable = False),
+			Column('evaluator', Integer, ForeignKey('User.id'), nullable = False),
 			Column('status',Enum('Waiting','Evaluated'), nullable = False, server_default = 'Waiting'),
 			Column('score',Integer, nullable = False, server_default= "0"),
 			Column('pnp',Enum('Pass','Nonpass'), nullable = False, server_default = 'Nonpass'),
-			Column('submitter', Unicode(30), ForeignKey('User.id'), nullable = False)
-			,autoload=True, autoload_with=engine
+			Column('submitter', Integer, ForeignKey('User.id'), nullable = False)
+			#,autoload=True, autoload_with=engine
 			)
 
 
 		#task data
 		
 		taskTableName = self.prefix + "_TaskData"
-		task = Table(taskTableName,metadata,
-			Column('id', Integer, primary_key=True),
+		self.tasktable = Table(taskTableName,metadata,
+			Column('id', Integer, primary_key=True,autoincrement = True),
 			Column('submittername', Unicode(100), nullable = False),
 			Column('parsedid', Integer, ForeignKey(parsedTableName+'.id'), nullable = False),
 			*(Column("sch_"+colname, Integer) for colname in mappinginfo)
-			,autoload=True, autoload_with=engine
+			#,autoload=True, autoload_with=engine
 			)
 
 		metadata.create_all(bind= engine)
@@ -94,15 +103,37 @@ class Task(Base):
 		self.original = type(originalTableName,(OriginalData,),{})
 		self.parsed = type(parsedTableName,(ParsedData,),{})
 		self.task = type(taskTableName,(TaskData,),{})
-		mapper(self.original, original)
-		mapper(self.parsed, parsed)
-		mapper(self.task, task)
+		mapper(self.original, self.originaltable)
+		mapper(self.parsed, self.parsedtable, properties={
+			'original': relationship(self.original, backref='parsed')
+		})
+		mapper(self.task, self.tasktable, properties={
+			'parsed': relationship(self.parsed, backref='task')
+		})
+
+		self.original.parsedclass = self.parsed
+		self.parsed.taskclass = self.task
 
 
 
 
-	def newOriginal(self,mappinginfo):
-		pass
+
+	def newOriginal(self, length, name,mappinginfo):
+		originalmodel = self.original(length, name,mappinginfo)
+		session.add(originalmodel)
+		session.commit()
+		return originalmodel
+
+
+
+	def getMapInfo(self):
+		info = list()
+		for col in self.tasktable.columns:
+			if col.name[:3]== "sch":
+				info.append({"name" : col.name, "type" : col.type })
+
+		return info
+		
 		
 
 
