aa7d8dfdddf82efa26aa79c2f3de9b0b14a3f422
Wonsup Yoon <pusnow@yonsei.ac.kr>
2015-12-03 20:27:14 +0900

    테스트 코드 갱신

--- test.py
+++ test.py
@@ -14,12 +14,15 @@ evaluator.role = u"평가자"
 
 submitter = User(u"submitter" + str(datetime.now()),u"김똘똘2",u"1234")
 
+
 session.add(evaluator)
 session.add(submitter)
+
 session.commit()
 
-t = Task("test","TS")
 
+
+t = Task("test",datetime.now(),"TS")
 
 d = ["data1","data2","data3"]
 
@@ -41,4 +44,16 @@ csvdata = u"""'1','45','32','23','21','312'
 '1','45','32','23','21','312'
 """
 csvtest = io.StringIO(csvdata)
-om.loadcsv(submitter,csvtest,1,datetime(2015, 1, 1),datetime(2016, 1, 1))
+pd = om.loadcsv(submitter,csvtest,1,datetime(2015, 1, 1),datetime(2016, 1, 1))
+
+pd.pnp = "Pass"
+
+print User.login(evaluator.loginid, u"1234h")
+
+
+
+
+
+
+
+
